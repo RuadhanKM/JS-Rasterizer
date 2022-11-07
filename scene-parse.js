54824ae7574622b9ac2d1569d261b9f5801e437e
RuadhanKM <9281239@students.k12.dc.us>
2022-11-07 11:09:42 -0500

    begin optomize

--- scene-parse.js
+++ scene-parse.js
@@ -2,9 +2,9 @@ async function getScene() {
     return fetch("scene.json").then(res => res.json()).then((obj) => {
         let scene = {}
 
-        scene.camPos = new vec3(obj.cam.pos.x, obj.cam.pos.y, obj.cam.pos.z)
-        scene.camPos = new vec3(obj.cam.rot.x, obj.cam.rot.y, obj.cam.rot.z)
-        scene.lightDir = new vec3(obj.lighting.dir.x, obj.lighting.dir.y, obj.lighting.dir.z)
+        scene.camPos = [obj.cam.pos.x, obj.cam.pos.y, obj.cam.pos.z]
+        scene.camPos = [obj.cam.rot.x, obj.cam.rot.y, obj.cam.rot.z]
+        scene.lightDir = [obj.lighting.dir.x, obj.lighting.dir.y, obj.lighting.dir.z]
         scene.scene = []
 
         let promises = []
@@ -14,10 +14,10 @@ async function getScene() {
                 promises.push(fetch(element.modelUrl).then(res => res.text()).then((obj) => {
                     let objFile = new OBJFile(obj).parse().models[0]
 
-                    objFile.rot = new vec3(element.rot.x, element.rot.y, element.rot.z)
-                    objFile.pos = new vec3(element.pos.x, element.pos.y, element.pos.z)
+                    objFile.rot = [element.rot.x, element.rot.y, element.rot.z]
+                    objFile.pos = [element.pos.x, element.pos.y, element.pos.z]
                     objFile.scale = element.size
-                    objFile.color = new vec3(255, 0, 0)
+                    objFile.color = [255, 0, 0]
                     objFile.name = element.name
 
                     scene.scene.push(objFile)
@@ -26,10 +26,10 @@ async function getScene() {
             if (element.type === "primative") {
                 let objFile = {}
 
-                objFile.rot = new vec3(element.rot.x, element.rot.y, element.rot.z)
-                objFile.pos = new vec3(element.pos.x, element.pos.y, element.pos.z)
+                objFile.rot = [element.rot.x, element.rot.y, element.rot.z]
+                objFile.pos = [element.pos.x, element.pos.y, element.pos.z]
                 objFile.scale = element.size
-                objFile.color = new vec3(255, 0, 0)
+                objFile.color = [255, 0, 0]
                 objFile.name = element.name
                 objFile.faces = getPrimativeFaces(element.primativeType)
 
